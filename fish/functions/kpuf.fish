77297da6cc801bcc7e1675fcc3b51b9da8dcb4fe
jorgechato <hack@jorgechato.com>
2024-10-08 09:47:00 +0900

    change(kpuf): add login to gcloud

--- fish/functions/kpuf.fish
+++ fish/functions/kpuf.fish
@@ -5,6 +5,8 @@ function kpuf
     eval_esc "$selected_namespace"
     set -l selected_ctx (yq eval '.[] | select(.alias == "'$selected_cluster'" or .cluster == "'$selected_cluster'").cluster' "$KPUF_CONFIG_FILE")
 
+    gke_gcloud_login $selected_ctx
+
     command kubectl config use-context $selected_ctx >/dev/null 2>/dev/null || echo "ERROR: You don't have $selected_ctx in your ~/.kube/config file."
     command kubectl config set-context --current --namespace=$selected_namespace >/dev/null 2>/dev/null
 end
@@ -18,5 +20,9 @@ end
 
 
 function gke_gcloud_login
-    # TODO: login if needed on context switch
+    set -l selected_metadata (string split _ $argv[1])
+    if contains "gke" $selected_metadata
+        gcloud auth login --no-launch-browser --update-adc
+        gcloud container clusters get-credentials $selected_metadata[4] --region $selected_metadata[3] --project $selected_metadata[2] >/dev/null 2>/dev/null || echo "ERROR: You don't have $selected_ctx in your ~/.kube/config file."
+    end
 end
