2ed238e77121a7d77f984a24b96559531839af65
jorgechato <hack@jorgechato.com>
2024-10-08 12:43:04 +0900

    change(nix): basic pkgs & global config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JORGE CHATO .dotfiles
 
-### Basic Installation
+## Basic Installation
 
 1. Install Nix
     ```shell
@@ -14,3 +14,17 @@
     ```shell
     $ nix-shell run nix-darwin --experimental-features 'nix-command flakes' -- switch --flake ~/.dotfiles/nix/darwin#ichi # change ichi with the device name
     ```
+
+## Add new package
+
+1. Add package to `environment.systemPackages` in `nix/flake.nix`
+2. Install package
+    ```shell
+    $ darwin-rebuild switch --flake ~/.dotfiles/nix#ichi # change ichi with the device name
+    ```
+
+## Common errors
+
+### error: path '/nix/store/pq2iwm2pxn19nikkxfk0b87p817lcn2l-source/nix/flake.nix' does not exist
+
+This error is due to the use of a git folder for nix flakes. To fix it, you need to commit your changes before running the command.
--- nix/flake.nix
+++ nix/flake.nix
@@ -5,48 +5,155 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [
-          pkgs.neovim
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+    let
+      configuration = { pkgs, config, ... }: {
+        nixpkgs.config.allowUnfree = true; # allow install paid apps (require to add license manually in each app)
+
+        environment.systemPackages =
+          [
+            # TERMINAL
+            pkgs.curl
+            pkgs.neovim
+            pkgs.mkalias
+            pkgs.nmap
+            pkgs.tree
+            pkgs.git
+            pkgs.fortune
+            pkgs.platinum-searcher
+            pkgs.fzf
+            pkgs.bat
+            pkgs.httpie
+            pkgs.grex
+            pkgs.direnv
+            pkgs.jq
+            pkgs.yq
+            pkgs.starship
+            pkgs.go
+            pkgs.protobuf
+            pkgs.jwt-cli
+            pkgs.gnused
+            pkgs.cue
+            pkgs.cuelsp
+            pkgs.rectangle
+            # FISH PLUGINS
+            pkgs.fishPlugins.z
+            pkgs.fishPlugins.fzf
+            # pkgs.fishPlugins.config
+            # pkgs.fishPlugins.direnv
+            # pkgs.fishPlugins.spark
+            # pkgs.fishPlugins.last-working-dir
+            # pkgs.fishPlugins.rustup
+            # pkgs.fishPlugins.https://github.com/blackjid/plugin-kubectl
+            # GUI
+            pkgs.iterm2
+            pkgs.iina
+            pkgs.vscode
+          ];
+
+        fonts.packages = [
+          pkgs.meslo-lgs-nf
         ];
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+        homebrew = {
+          enable = true;
+          brews = [
+            "mas"
+          ];
+          casks = [
+            "the-unarchiver"
+            "discrod"
+            "docker"
+            "1password"
+            "brave-browser"
+            "jetbrains-toolbox"
+          ];
+          # masApps = [];
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        };
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # Add apps to spotlight for mac
+        system.activationScripts.applications.text =
+          let
+            env = pkgs.buildEnv {
+              name = "system-applications";
+              paths = config.environment.systemPackages;
+              pathsToLink = "/Applications";
+            };
+          in
+          pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read src; do
+              app_name=$(basename "$src")
+              echo "copying $src" >&2
+              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+          '';
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.fish.enable = true;
+        system.defaults = {
+          dock.autohide = true;
+          dock.orientation = "right";
+          dock.persistent-apps = [
+            "/Applications/Brave Browser.app"
+            "${pkgs.iterm2}/Applications/iTerm.app"
+          ];
+          finder.FXPreferredViewStyle = "Nlsv";
+          NSGlobalDomain.AppleICUForce24HourTime = true;
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          NSGlobalDomain."com.apple.keyboard.fnState" = true;
+        };
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.keyboard = {
+          enableKeyMapping = true;
+          remapCapsLockToControl = true;
+        };
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
-      # nixpkgs.hostPlatform = "aarch64-darwin"; # for Apple Silicon
+        nix.settings.experimental-features = "nix-command flakes";
+
+        programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "x86_64-darwin"; # for Apple Silicon use "aarch64-darwin"
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."ichi" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              user = "jorge";
+              autoMigrate = true; # Automatically migrate existing Homebrew installations
+            };
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."ichi".pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."ichi" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."ichi".pkgs;
-  };
 }
