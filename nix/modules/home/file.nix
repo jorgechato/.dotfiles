69c915e8f076e5d19216c78905f5cc9890f8606e
jorgechato <hack@jorgechato.com>
2024-11-01 16:39:37 +0900

    fix(nix): .files on recusive

--- nix/modules/home/file.nix
+++ nix/modules/home/file.nix
@@ -2,10 +2,22 @@
 
 {
   home.file = {
-    ".config/1Password".source = ~/.dotfiles/1Password;
-    ".config/fish".source = ~/.dotfiles/fish;
-    ".config/nvim".source = ~/.dotfiles/nvim;
-    ".config/skhd".source = ~/.dotfiles/skhd;
+    ".config/1Password" = {
+      source = ~/.dotfiles/1Password;
+      recursive = true;
+    };
+    ".config/fish" = {
+      source = ~/.dotfiles/fish;
+      recursive = true;
+    };
+    ".config/nvim" = {
+      source = ~/.dotfiles/nvim;
+      recursive = true;
+    };
+    ".config/skhd" = {
+      source = ~/.dotfiles/skhd;
+      recursive = true;
+    };
     ".config/starship.toml".source = ~/.dotfiles/starship.toml;
     ".gitconfig".source = ~/.dotfiles/.gitconfig;
     ".editorconfig".source = ~/.dotfiles/.editorconfig;
