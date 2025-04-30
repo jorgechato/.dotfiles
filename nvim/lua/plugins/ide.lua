885c7be89f219895633d318a3ee72ef586b0939c
jorgechato <hack@jorgechato.com>
2025-05-01 08:27:10 +0900

    change(nvim): move md to it's own file

--- nvim/lua/plugins/ide.lua
+++ nvim/lua/plugins/ide.lua
@@ -106,12 +106,6 @@ return {
         end,
     },
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    },
-    {
         "folke/todo-comments.nvim",
         enabled = true,
         dependencies = "nvim-lua/plenary.nvim",
--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -13,6 +13,12 @@ return {
         opts = {},
     },
     {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
         'nvim-telekasten/telekasten.nvim',
         dependencies = { 'nvim-telescope/telescope-media-files.nvim' },
         lazy = false,
