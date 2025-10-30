399ae2f9c8cbb169abd0c6f930843078c7257383
jorgechato <hack@jorgechato.com>
2025-10-30 17:54:01 +0900

    change: add mongodb-cli to work

--- nix/modules/nixos/pkgs.nix
+++ nix/modules/nixos/pkgs.nix
@@ -108,10 +108,11 @@
         pkgs.grpcurl
         pkgs.gosec
         pkgs.pwgen
-        pkgs.php81
+        # pkgs.php81
         pkgs.telepresence2
         pkgs.zoom-us
         pkgs.notion-app
+        pkgs.mongodb-cli
         (pkgs.google-cloud-sdk.withExtraComponents [
           pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
         ])
